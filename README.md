05345262b13a314ab3883bb1ea275ce7f68a4b13
zybeasy <zybeasy@163.com>
2020-03-09 00:29:14 +0800

    2020年03月 9日  0:29:17

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # JavaDemo
 ## 二级标题
 Demo for learning Java.
+Test  WhiteSpace  sfe   df  df  
+df
--- src/main/java/com/zy/java/demo/Main.java
+++ src/main/java/com/zy/java/demo/Main.java
@@ -4,6 +4,9 @@ import com.zy.java.demo.tij.generator.BasicGenerator;
 import com.zy.java.demo.tij.generator.Generator;
 import com.zy.java.demo.tij.widget.CountedObject;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Main
 {
     public static void main(String[] asrgs)
@@ -13,5 +16,16 @@ public class Main
         Generator<CountedObject> gen = BasicGenerator.create(CountedObject.class);
         for (int i = 0; i < 5; ++i)
             System.out.println(gen.next());
+
+        System.out.println("FUCK THE WORLD END");
+
+
+        String a = "Hello World";
+        String b = "ZHANG";
+        int[] arr = {1, 2, 3};
+        List list = Arrays.asList(arr);
+        System.out.println(list.size());
+        System.out.println(list.get(0));
+        System.out.println(Arrays.toString(arr));
     }
 }
